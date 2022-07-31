93b0c0eb38a377e36b53d8cb42a55392d1e497be
Lee, Jaehyeok <dlwogur15@gmail.com>
2022-07-31 19:21:15 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -3,33 +3,12 @@ import './App.css';
 
 function App() {
 
-  let player = [2,5,10];
 
-  const replay = () => {
-    for(let i in player) {
-      console.log(player[i]);
-    }
-  }
 
   return (
 
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-
-      {replay()}
+      css 연습
     </div>
   );
 }
