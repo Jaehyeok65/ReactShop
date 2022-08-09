5a0c8e7b85717bfb7c9e73d14d2649431087a6fe
Lee, Jaehyeok <dlwogur15@gmail.com>
2022-08-10 06:09:54 +0900

    useRef로 스크롤바 컨트롤 추가

--- src/Component/ShopList.jsx
+++ src/Component/ShopList.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ListCard from './ListCard';
 import styles from './List.module.css';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import  Pagination  from './Pagegination';
+import { useEffect } from 'react';
 
 
 
@@ -10,16 +11,22 @@ import  Pagination  from './Pagegination';
 
 const ShopList = ( { Goods }) => {
 
+  const scrollref = useRef();
   const [limit, setLimit] = useState(12);
   const [page, setPage] = useState(1);
   const offset = (page - 1) * limit;
+
+  useEffect( () => {
+    scrollref.current.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' });
+  },[page])
+  
 
 
 
 
 
     return (
-      <div>
+      <div ref={scrollref}>
         <div className={styles.shop}>
           { Goods.slice(offset,offset + limit).map( (Good, index) => (
             <ListCard key = {index} url = {Good.url} price = {Good.price} name = {Good.name} />
